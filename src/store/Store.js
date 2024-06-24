ad0baef16aedaff0717102b88cf27cacc796ac66
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-06-24 19:17:10 +0500

    Update Store.js

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import toDoReducer from './toDoSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import toDoReducer from "./toDoSlice";
 
 export default configureStore({
   reducer: {
     todos: toDoReducer,
   },
 });
+
+// store component
